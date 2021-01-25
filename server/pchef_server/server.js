8cee4166a4d1ab30c071fc145bdcd61220b12127
Ricardo Aranaga <raa010@email.latech.edu>
2021-01-25 04:34:44 -0600

    server implementation

---
+++ server/pchef_server/server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+var sqlCon = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'12345',
+    database:'DB_Recipe_Remote'
+});
+
+var app = express();
+app.use("/", express.static("../public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/recipes",(req,res,next)=>{
+    
+    var query = "SELECT r.RName AS 'Recipe',r.Instructions, ri.Amount AS 'Amount', mu.Measure AS 'Unit of Measure', i.FName AS 'Ingredient' FROM RECIPE r JOIN REC_INGREDIENT ri on r.id = ri.RIID JOIN FOOD i on i.FID = ri.Food_ID LEFT OUTER JOIN MEASURE mu on mu.MID = Measure_ID";
+
+    sqlCon.query(query,function(error,result,fields){
+        sqlCon.on('error',function(err){
+            console.log('[MYSQL]ERROR',err);
+        });
+        if(result && result.length) {
+            res.end(JSON.stringify(result));
+        }
+        else {
+            res.end(JSON.stringify('no recipes available'));
+        }
+    });
+});
+
+app.get("/search",(req,res,next)=>{
+    var post_data = req.body;
+    var recipe_search = post_data.search;
+    console.log(recipe_search);
+
+    var query = "select * from RECIPE where RName = '"+recipe_search+"'";
+
+    sqlCon.query(query,function(error,result,fields){
+        sqlCon.on('error',function(err){
+            console.log('[MYSQL]ERROR',err);
+        });
+        if(result && result.length) {
+            res.end(JSON.stringify(result));
+        }
+        else {
+            res.end(JSON.stringify('no recipes available'));
+        }
+    });
+});
+
+
+
+app.listen(3000,()=>{
+    console.log("Running REST API on port 3000")
+})
