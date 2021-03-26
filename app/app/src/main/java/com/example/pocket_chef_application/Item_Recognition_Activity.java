5b0a4054224711c3d7388c99cef83de06ce84d40
Jacob-Bordelon <jab145@latech.edu>
2021-03-26 18:23:38 -0500

    working on firestore

---
+++ app/app/src/main/java/com/example/pocket_chef_application/Item_Recognition_Activity.java
@@ -0,0 +1,14 @@
+package com.example.pocket_chef_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Item_Recognition_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item__recognition_);
+    }
+}
