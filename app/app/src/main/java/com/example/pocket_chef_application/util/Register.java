ac72329d2d5c80b36fc9a746395042a5a6f12ca0
Linh Nguyen <tuonglinhchelsea1@gmail.com>
2021-04-17 21:54:31 -0500

    Create the register and log in activity along with designing the pages

---
+++ app/app/src/main/java/com/example/pocket_chef_application/util/Register.java
@@ -0,0 +1,15 @@
+package com.example.pocket_chef_application.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.pocket_chef_application.R;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
