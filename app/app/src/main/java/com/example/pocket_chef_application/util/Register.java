60ec40c7fcf5665b8afbf6d3da897b15a4ab9de7
Linh Nguyen <tuonglinhchelsea1@gmail.com>
2021-04-18 19:58:24 -0500

    Change method to input Date of Birth and add check point for password confirmation

--- app/app/src/main/java/com/example/pocket_chef_application/util/Register.java
+++ app/app/src/main/java/com/example/pocket_chef_application/util/Register.java
@@ -9,20 +9,28 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class Register extends AppCompatActivity implements View.OnClickListener{
 
     private TextView registerUser;
-    private EditText editTextFullName, editTextAge, editTextEmail, editTextPassword;
+    private EditText editTextFullName, editTextAge, editTextEmail, editTextPassword, editTextPasswordConfirm;
     private ProgressBar progressBar;
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     private FirebaseAuth mAuth;
 
@@ -37,11 +45,41 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         registerUser.setOnClickListener(this);
 
         editTextFullName = (EditText) findViewById(R.id.fullName);
-        editTextAge = (EditText) findViewById(R.id.age);
+/*        editTextAge = (EditText) findViewById(R.id.DOB);*/
         editTextEmail = (EditText) findViewById(R.id.email);
         editTextPassword = (EditText) findViewById(R.id.password);
+        editTextPasswordConfirm = (EditText) findViewById(R.id.confirmPassword);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mDisplayDate = (TextView) findViewById(R.id.DOB);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        Register.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+
+                String date = month + "/" + dayOfMonth + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
     }
 
     @Override
@@ -56,8 +94,9 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
     private void RegisterUser() {
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+        String confirmation = editTextPasswordConfirm.getText().toString().trim();
         String fullName = editTextFullName.getText().toString().trim();
-        String age = editTextAge.getText().toString().trim();
+        String age = mDisplayDate.getText().toString().trim();
 
         if(fullName.isEmpty()){
             editTextFullName.setError("Full name is required!");
@@ -91,6 +130,12 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
 
         if(password.length() < 6){
             editTextPassword.setError("Min password length should be 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(!password.equals(confirmation)){
+            editTextPassword.setError("The password confirmation must match the password!");
             editTextPassword.requestFocus();
             return;
         }
